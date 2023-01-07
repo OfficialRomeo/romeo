9a594221a10e18d4066504d2ba3c101ebb0426ad
Romeo <98103853+OfficialRomeo@users.noreply.github.com>
2023-01-06 21:51:08 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+
+goSite
+
+button1.onClick = goSite
+button2.onClick = goSite
+button3.onClick = goSite
